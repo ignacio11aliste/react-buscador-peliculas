832f62138110fe00cb4c58a072cfc924b3bc1603
Ignacio Aliste <aliste11toledo@gmail.com>
2023-10-09 23:40:55 -0300

    Creacion componente Movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
 import responseMovies from './mocks/with-results.json'
 import withoutResult from './mocks/no-result.json'
+import './App.css'
+
+import { Movies } from './components/Movies'
 
 function App() {
   const movies = responseMovies.Search
-  const hasMovies = movies?.length > 0
-
   return (
     <div className='page'>
       <header>
@@ -17,19 +17,7 @@ function App() {
       </header>
 
       <main>
-        {hasMovies ? (
-          <ul>
-            {movies.map(movie => (
-              <li key={movie.imdbID}>
-                <h3>{movie.Title}</h3>
-                <p>{movie.Year}</p>
-                <img src={movie.Poster} alt={movie.Title} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No se encontraron resultados para esta busqueda</p>
-        )}
+        <Movies movies={movies} />
       </main>
     </div>
   )
